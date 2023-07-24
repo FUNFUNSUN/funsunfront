cab750e9688d0c15828cac0bba6ea76cecf2aab4
WONYUNSUN <ereunosun113@gmail.com>
2023-07-24 17:31:59 +0900

    Create: add explore, faq, terms

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class ExlporeScreen extends StatelessWidget {
+  const ExlporeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+                    suffixIcon: const Icon(Icons.search_rounded),
+                    filled: true,
+                    fillColor: const Color(0xFFD9D9D9),
+                    hintText: '검색',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '전체공개펀딩',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  width: 325,
+                  height: 222,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color(0xFFE7F5F6),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '참여한 펀딩',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  width: 325,
+                  height: 222,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color(0xFFE7F5F6),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/faq_secreen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class FaqScreen extends StatelessWidget {
+  const FaqScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'FAQ',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    '자주 물어보는 질문을 확인하세요',
+                    style: TextStyle(color: Color(0xff7D7D7D)),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Column(children: [
+                    Faqwidget(
+                      question: '질문1',
+                      answer: '답변1',
+                    ),
+                    Faqwidget(question: '질문2', answer: '답변2'),
+                  ]),
+                  InkWell(
+                    onTap: () {
+                      print('클릭');
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: const Color(0xffD9D9D9),
+                            borderRadius: BorderRadius.circular(15)),
+                        width: 400,
+                        height: 50,
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '다음',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 15),
+                            )
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Faqwidget extends StatelessWidget {
+  final String question, answer;
+  const Faqwidget({
+    super.key,
+    required this.question,
+    required this.answer,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      collapsedTextColor: Colors.black,
+      leading: const Icon(Icons.help_outline_rounded),
+      title: Text(question),
+      trailing: const Icon(
+        Icons.arrow_drop_down,
+        color: Colors.black,
+      ),
+      backgroundColor: Colors.white,
+      children: <Widget>[
+        ListTile(
+          title: Text(answer),
+          onTap: () {
+            // Do something when item 1 is tapped
+          },
+        ),
+      ],
+    );
+  }
+}
