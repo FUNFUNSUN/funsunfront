344310562d07121bee88a4fb60baece3783de864
DevP3PP3R <petercater@naver.com>
2023-07-25 17:27:20 +0900

    Rename : main-> home, home-> First

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: Scaffold(
+        body: FirstScreen(),
+      ),
     );
   }
 }
