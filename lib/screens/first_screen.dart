b679c8e495c46cb445e48e7c2d31f23bd3e8fce5
DevP3PP3R <petercater@naver.com>
2023-07-26 16:01:31 +0900

    Feat : RefreshIndicator on Home(screen, list)

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:funsunfront/screens/bottom_nav_shortcuts.dart';
 import 'package:funsunfront/screens/edit_screen.dart';
 import 'package:funsunfront/screens/searchresult_screen.dart';
 import 'package:funsunfront/screens/view_screen.dart';
@@ -64,6 +65,16 @@ class FirstScreen extends StatelessWidget {
                     );
                   },
                   child: const Text('SearchResultScreen 이동 임시테스트'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const BottomNavShortcuts()),
+                    );
+                  },
+                  child: const Text('메인화면 이동 임시테스트'),
                 ),
               ],
             ),
