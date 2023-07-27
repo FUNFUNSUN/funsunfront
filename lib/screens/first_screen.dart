83cb51dd2ae7b3310040d167f192b117cd62022e
WONYUNSUN <ereunosun113@gmail.com>
2023-07-27 16:41:35 +0900

    Merge : firstpage import

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:funsunfront/screens/bottom_nav_shortcuts.dart';
 import 'package:funsunfront/screens/edit_screen.dart';
+import 'package:funsunfront/screens/funding_screen.dart';
+import 'package:funsunfront/screens/publicsupport_screen.dart';
 import 'package:funsunfront/screens/searchresult_screen.dart';
 import '../services/kakao_login_button.dart';
 import 'faq_screen.dart';
@@ -84,21 +86,6 @@ class FirstScreen extends StatelessWidget {
                     );
                   },
                   child: const Text('펀딩 게시글 테스트'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const PublicSupportScreen(
-                                FundingExpireDate: '2023년7월27일',
-                                FundingTitle: '펀딩제목이~~머냐면~~~어쩌구',
-                                FundingImage:
-                                    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FczFu0l%2FbtrqAb95DTz%2FEzH59ItsvNjSB8JEOEarkK%2Fimg.png',
-                              )),
-                    );
-                  },
-                  child: const Text('PublicSupportScreen 이동 임시테스트'),
                 ),
                 TextButton(
                   onPressed: () {
