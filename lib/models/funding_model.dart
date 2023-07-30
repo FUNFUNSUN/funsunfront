369745e17bb9f1db98a6409885b57ced213d63d5
DevP3PP3R <petercater@naver.com>
2023-07-30 18:20:53 +0900

    Fix : createdOn and image can be null

--- lib/models/funding_model.dart
+++ lib/models/funding_model.dart
@@ -1,7 +1,8 @@
 class FundingModel {
   final int id, goalAmount, currentAmount;
-  final bool public;
-  final String title, content, expireOn, createdOn, image;
+  final bool? public;
+  final String title, content, expireOn;
+  final String? createdOn, image;
   final Map<String, dynamic> author;
 
   FundingModel.fromJson(Map<String, dynamic> json)
