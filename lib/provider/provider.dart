7774310d78d9cec1bcaa83ed7364a6072362fd12
DevP3PP3R <petercater@naver.com>
2023-07-31 20:26:11 +0900

    Test : currentUser

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class SignInProvider extends ChangeNotifier {
   bool _signIn = false;
-
   bool get signIn => _signIn;
+  String? _currentUser = '';
+  String? get currentUser => _currentUser;
 
   void setTrue() {
     _signIn = true;
@@ -12,6 +13,11 @@ class SignInProvider extends ChangeNotifier {
 
   void setFalse() {
     _signIn = false;
+    notifyListeners();
+  }
+
+  void setCurrentUser(String uid) {
+    _currentUser = uid;
     notifyListeners();
   }
 }
