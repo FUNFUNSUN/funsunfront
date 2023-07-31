d4e31f2ed864423f9df93af0bd5e6434e78b4059
WONYUNSUN <ereunosun113@gmail.com>
2023-08-01 02:03:29 +0900

    Fix : public/mysupport overflow issue

--- lib/screens/mysupport_screen.dart
+++ lib/screens/mysupport_screen.dart
@@ -16,32 +16,30 @@ class MySupportScreen extends StatelessWidget {
     final sizeY = MediaQuery.of(context).size.height;
     return MaterialApp(
       home: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  '서포트한 펀딩',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 50, left: 15, right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '서포트한 펀딩',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
-                  height: sizeY,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Expanded(
+                child: SizedBox(
                   width: sizeX,
+                  height: sizeY,
                   child: FutureBuilder(
                     future: fundings,
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         // 데이터를 불러오는 동안 로딩 표시
-                        return const Center(child: CircularProgressIndicator());
+                        return const CircularProgressIndicator();
                       } else if (snapshot.hasError) {
                         // 오류 표시
                         return Text('오류: ${snapshot.error}');
@@ -54,8 +52,8 @@ class MySupportScreen extends StatelessWidget {
                     },
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
