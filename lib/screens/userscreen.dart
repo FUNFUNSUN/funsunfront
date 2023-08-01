29dd5185572fd62f9bfebf717f37bf0e08d36bf0
WONYUNSUN <ereunosun113@gmail.com>
2023-08-01 18:39:22 +0900

    Fix : Delete hardcording in user/myscreen

--- lib/screens/userscreen.dart
+++ lib/screens/userscreen.dart
@@ -24,20 +24,6 @@ class UserScreen extends StatelessWidget {
     final sizeX = MediaQuery.of(context).size.width;
     // final sizeY = MediaQuery.of(context).size.height;
 
-    List<String> imgUrls = [];
-    imgUrls.add(
-        'https://flexible.img.hani.co.kr/flexible/normal/970/970/imgdb/original/2023/0619/20230619501341.jpg');
-    imgUrls.add(
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSNkiSUcQ1o4jzsNDFSNYE1Bt3xmRZK3joQ&usqp=CAU');
-    imgUrls.add(
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtviSR-KwPyKiV_mJTGqtjgzzVV8r3Z5tRmXTjoypCsKLpVZPa4OuENBO5xcJ6mva1Sxc&usqp=CAU');
-    imgUrls.add(
-        'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202303/19/starnews/20230319084657800lhwc.jpg');
-    imgUrls.add(
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtviSR-KwPyKiV_mJTGqtjgzzVV8r3Z5tRmXTjoypCsKLpVZPa4OuENBO5xcJ6mva1Sxc&usqp=CAU');
-    imgUrls.add(
-        'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202303/19/starnews/20230319084657800lhwc.jpg');
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
