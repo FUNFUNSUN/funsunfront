a55a747ff535c45c7b71cd951c920bd29835741b
WONYUNSUN <ereunosun113@gmail.com>
2023-08-02 22:01:38 +0900

    Fix : Kill MaterialApp. Material App is my enemy

--- lib/screens/searchresult_screen.dart
+++ lib/screens/searchresult_screen.dart
@@ -10,7 +10,7 @@ class SearchResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SearchBox());
+    return const SearchBox();
   }
 }
 
