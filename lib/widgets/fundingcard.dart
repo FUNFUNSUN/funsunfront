a3cc36141c80f67b31a4e593c8223d035e7526f5
WONYUNSUN <ereunosun113@gmail.com>
2023-08-04 15:04:17 +0900

    Create : expired_ribbon Widget

--- lib/widgets/fundingcard.dart
+++ lib/widgets/fundingcard.dart
@@ -23,6 +23,7 @@ class FundingCard extends StatelessWidget {
             padding: const EdgeInsets.fromLTRB(5, 0, 5, 20),
             itemCount: fundings.length,
             physics: const AlwaysScrollableScrollPhysics(),
+
             // shrinkWrap: true,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
@@ -31,98 +32,107 @@ class FundingCard extends StatelessWidget {
               crossAxisSpacing: 10,
             ),
             itemBuilder: (BuildContext context, int index) {
+              //만료확인변수
+              bool isExpired = DateTime.parse(fundings[index].expireOn)
+                  .isBefore(DateTime.now());
               return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FundingScreen(
-                              id: fundings[index].id.toString(),
-                            )),
-                  );
-                },
-                child: Container(
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 0,
-                            blurRadius: 5.0,
-                            offset: const Offset(1, .8))
-                      ]),
-                  child: Column(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FundingScreen(
+                                id: fundings[index].id.toString(),
+                              )),
+                    );
+                  },
+                  child: Stack(
                     children: [
-                      Expanded(
-                        flex: 8,
-                        child: SizedBox(
-                          width: sizeX,
-                          child: FittedBox(
-                            fit: BoxFit.cover,
-                            child: (fundings[index].image != null)
-                                ? Image.network(
-                                    '$imgBaseUrl${fundings[index].image}',
-                                    //펀딩이미지
-                                    fit: BoxFit.cover,
-                                  )
-                                : Image.asset(
-                                    'assets/images/default_funding.jpg',
-                                    fit: BoxFit.cover,
-                                  ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Container(
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  fundings[index].title,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500),
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 0,
+                                  blurRadius: 5.0,
+                                  offset: const Offset(1, .8))
+                            ]),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 8,
+                              child: SizedBox(
+                                width: sizeX,
+                                child: FittedBox(
+                                  fit: BoxFit.cover,
+                                  child: (fundings[index].image != null)
+                                      ? Image.network(
+                                          '$imgBaseUrl${fundings[index].image}',
+                                          //펀딩이미지
+                                          fit: BoxFit.cover,
+                                        )
+                                      : Image.asset(
+                                          'assets/images/default_funding.jpg',
+                                          fit: BoxFit.cover,
+                                        ),
                                 ),
-                                Row(
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        const Text(
-                                          '펀딩종료일',
-                                          style: TextStyle(
-                                            fontSize: 11,
-                                          ),
-                                        ),
-                                        Text(
-                                          style: const TextStyle(
-                                            fontSize: 12,
-                                          ),
-                                          DateFormat.yMMMd('en_US').format(
-                                              DateTime.parse(
-                                                  fundings[index].expireOn)),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                )
-                              ],
+                              ),
                             ),
-                          ),
+                            Expanded(
+                              flex: 4,
+                              child: Container(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        fundings[index].title,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const Text(
+                                                '펀딩종료일',
+                                                style: TextStyle(
+                                                  fontSize: 11,
+                                                ),
+                                              ),
+                                              Text(
+                                                style: const TextStyle(
+                                                  fontSize: 12,
+                                                ),
+                                                DateFormat.yMMMd('en_US')
+                                                    .format(DateTime.parse(
+                                                        fundings[index]
+                                                            .expireOn)),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
-                  ),
-                ),
-              );
+                  ));
             },
           )
         : Center(
