7592fe8960180a91edec0e375dfa5e01fbf8ffd0
DevP3PP3R <petercater@naver.com>
2023-08-07 15:57:56 +0900

    Design : preview vertical padding Layout

--- lib/screens/preview_screen.dart
+++ lib/screens/preview_screen.dart
@@ -17,12 +17,12 @@ class PreviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    //final screenHeight = MediaQuery.of(context).size.height;
     bool flag = true;
 
     DateTime KoreaDateTime = DateTime.now().add(const Duration(hours: 9));
-    print('한국 시간 $KoreaDateTime');
-    print('펀딩시간 ${DateTime.parse(temp['expire_on'])}');
+    //print('한국 시간 $KoreaDateTime');
+    //print('펀딩시간 ${DateTime.parse(temp['expire_on'])}');
 
     final ex =
         DateTime.parse(temp['expire_on']).difference(KoreaDateTime).toString();
@@ -30,12 +30,12 @@ class PreviewScreen extends StatelessWidget {
     int tempDifference = int.parse((ex.substring(0, ex.indexOf(':'))));
 
     final leftDays = tempDifference ~/ 24;
-    print('차이나는 날짜만 출력 : $leftDays');
+    //print('차이나는 날짜만 출력 : $leftDays');
 
     final leftHours = tempDifference - leftDays * 24;
-    print('차이나는 시간만 출력 : $leftHours');
+    //print('차이나는 시간만 출력 : $leftHours');
 
-    const String baseurl = 'http://projectsekai.kro.kr:5000/';
+    //const String baseurl = 'http://projectsekai.kro.kr:5000/';
     FundingsProvider fundingsProvider =
         Provider.of<FundingsProvider>(context, listen: false);
     UserProvider userProvider =
@@ -44,111 +44,112 @@ class PreviewScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(children: [
-              const SizedBox(
-                height: 60,
-              ),
-              const Text(
-                '펀딩 내용을 확인해주세요',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                '업로드 전 미리 확인하는 페이지입니다.',
-                style: TextStyle(color: Color(0xff7D7D7D)),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: screenWidth * 0.8,
-                height: screenWidth * 0.8,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Theme.of(context).primaryColorDark.withOpacity(0.6),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
                 ),
-                clipBehavior: Clip.hardEdge,
-                child: (image != null)
-                    ? Image(
-                        image: FileImage(image!),
-                        fit: BoxFit.cover,
-                      )
-                    : Image.asset('assets/images/default_profile.jpg',
-                        fit: BoxFit.cover),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              AchievementRate(
-                percent: 0,
-                date: leftDays,
-                hour: leftHours,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    temp['title'],
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w900,
+                const Text(
+                  '펀딩 내용을 확인해주세요',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  '업로드 전 미리 확인하는 페이지입니다.',
+                  style: TextStyle(color: Color(0xff7D7D7D)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: screenWidth * 0.8,
+                  height: screenWidth * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Theme.of(context).primaryColorDark.withOpacity(0.6),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: (image != null)
+                      ? Image(
+                          image: FileImage(image!),
+                          fit: BoxFit.cover,
+                        )
+                      : Image.asset('assets/images/default_profile.jpg',
+                          fit: BoxFit.cover),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                AchievementRate(
+                  percent: 0,
+                  date: leftDays,
+                  hour: leftHours,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      temp['title'],
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    temp['content'],
-                    style: const TextStyle(
-                      fontSize: 16,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      temp['content'],
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: InkWell(
-                  onTap: () async {
-                    if (flag) {
-                      // 실제로 펀딩 작성 버튼 누르면 실행되는 부분
-                      flag = false;
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: InkWell(
+                    onTap: () async {
+                      if (flag) {
+                        // 실제로 펀딩 작성 버튼 누르면 실행되는 부분
+                        flag = false;
 
-                      // postResult에 API결과 담아주고
-                      Map<String, dynamic> postResult =
-                          await Funding.postFunding(
-                              fundingData: temp, image: image);
+                        // postResult에 API결과 담아주고
+                        Map<String, dynamic> postResult =
+                            await Funding.postFunding(
+                                fundingData: temp, image: image);
 
-                      if (context.mounted) {
-                        // 펀딩 작성 후에는 맨 밑까지 터트리고 펀딩 화면으로 이동
-                        Navigator.of(context)
-                            .popUntil((route) => route.isFirst);
-                        // 내 펀딩 목록에 추가해주고
-                        fundingsProvider.getMyfundings(userProvider.user!.id);
-                        // 펀딩 상세 페이지에도 추가해줌
-                        fundingsProvider
-                            .getFundingDetail(postResult['id'].toString());
-                        // 전체공개 펀딩 목록에도 추가해줌
-                        fundingsProvider.getPublicFundings();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FundingScreen(
-                                  id: postResult['id'].toString())),
-                        );
+                        if (context.mounted) {
+                          // 펀딩 작성 후에는 맨 밑까지 터트리고 펀딩 화면으로 이동
+                          Navigator.of(context)
+                              .popUntil((route) => route.isFirst);
+                          // 내 펀딩 목록에 추가해주고
+                          fundingsProvider.getMyfundings(userProvider.user!.id);
+                          // 펀딩 상세 페이지에도 추가해줌
+                          fundingsProvider
+                              .getFundingDetail(postResult['id'].toString());
+                          // 전체공개 펀딩 목록에도 추가해줌
+                          fundingsProvider.getPublicFundings();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FundingScreen(
+                                    id: postResult['id'].toString())),
+                          );
+                        }
                       }
-                    }
-                  },
-                  child: Container(
+                    },
+                    child: Container(
                       decoration: BoxDecoration(
                           color: Theme.of(context).primaryColor,
                           borderRadius: BorderRadius.circular(15)),
@@ -162,10 +163,15 @@ class PreviewScreen extends StatelessWidget {
                               fontSize: 20,
                               color: Colors.white),
                         ),
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ]),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
           ),
         ),
       ),
